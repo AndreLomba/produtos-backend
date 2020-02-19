524d77a8bea24131f3ad1e972a1b552360c45e58
André Lomba <landrelomba@gmail.com>
2020-02-19 01:20:10 +0000

    Complete crud express

--- index.js
+++ index.js
@@ -12,6 +12,10 @@ server.get('/produto', function(request, response) {
     return response.json(produtos);
 });
 
+// MIDDLEWARES
+server.use(express.json());
+
+// RECEBE O ID DO PRODUTO COMO PARAMETRO, FILTRA O ID E RETORNA
 server.get('/produto/:id', function(request, response) {
 
     const id = request.params.id;
@@ -19,8 +23,46 @@ server.get('/produto/:id', function(request, response) {
     const produto = produtos.filter(p => p.id == id);
 
     return response.json(produto);
-    
 });
 
+server.post('/produto', function(request, response){
+    const produto = request.body;
+    produtos.push(produto);
+
+    var nome = request.body.nome;
+
+    return response.status(201).send();
+});
+
+server.delete('/produto/:id', function(request, response){
+    const id = request.params.id;
+
+    const produto = produtos.filter(p => p.id != id);
+
+    for (var i = 0; i < produtos.length; i++) {
+        if(produtos[i].id == id){
+            var nome = produtos[i].nome;
+            produtos.splice(i,1);
+
+        }
+    }
+
+    return response.json('Apagou o produto '+ nome);
+});
+
+server.put('/produto/:id', (req, res) => {
+    var id = req.params.id;
+    
+    const produto = req.body;
+
+    produtos.forEach(p => {
+        if(p.id == id){
+            p.nome = produto.nome;
+            p.preco = produto.preco;
+        }
+    })
+
+    return res.send();
+});
 
 server.listen(3000);
